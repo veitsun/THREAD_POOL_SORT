3cdf658c1928a83625b9d7ae6d709f753529f825
sunwei <sw20002000@163.com>
2024-09-19 21:46:11 +0800

    修改cmake文件

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
-// #include "../include/CGemmWithC.h"
-// #include "CMatrixData.h"
+#include "../include/CGemmWithC.h"
+#include "CMatrixData.h"
 #include <algorithm>
 #include <cmath>
 #include <cstdio>
 #include <cstring>
+#include <iostream>
 #include <vector>
-// #include <iostream>
 
 void convert_vector_to_pointer(const std::vector<float> &vec, float *ptr) {
   // 使用 std::copy 复制 vector 数据到指针
@@ -14,7 +14,6 @@ void convert_vector_to_pointer(const std::vector<float> &vec, float *ptr) {
 
 int main(int argc, char **argv) {
 
-  /*
   // M , N , K, aplha , A, B, beta, C
   CGemmWithC girl;
   CMatrixData matrixdataA(5, 5);
@@ -23,29 +22,27 @@ int main(int argc, char **argv) {
   matrixdataA.createData();
   matrixdataB.createData();
   matrixdataC.createData();
-  //matrixdata.show();
+  // matrixdata.show();
   float *A = new float[matrixdataA.getdata().size()];
   float *B = new float[matrixdataB.getdata().size()];
   float *C = new float[matrixdataC.getdata().size()];
   // 将 vector 数据复制到指针
-convert_vector_to_pointer(matrixdataA.getdata(), A);
+  convert_vector_to_pointer(matrixdataA.getdata(), A);
   convert_vector_to_pointer(matrixdataB.getdata(), B);
   convert_vector_to_pointer(matrixdataC.getdata(), C);
   girl.solveProblem(5, 5, 5, 1.0, &A, &B, 1.0, &C);
-
 
   std::copy(C, C + 25, matrixdataC.getdata().begin());
   matrixdataA.show();
   matrixdataB.show();
   matrixdataC.show();
 
-  //for(int i = 0; i < )
+  // for(int i = 0; i < )
 
   std::cout << "sunwei over" << std::endl;
   delete[] A;
   delete[] B;
   delete[] C;
-  */
 
   return 0;
 }
